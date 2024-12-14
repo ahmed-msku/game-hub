9c9b5c230e4ca1d5c88715c0becbe0c3dc1af9a3
Ahmed <ahmed@gmail.com>
2024-12-13 19:21:31 -0800

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import NoImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return NoImage;
 
   return url.replace("media/", "media/crop/600/400/");
 };
